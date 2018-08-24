5ecdea8792610aed3521cc4800d0d1d4d01bf2c0
Peter Gagliardi <ptrgags@gmail.com>
2018-08-24 01:31:50 +0000

    Compute bounding box of text

--- layout.py
+++ layout.py
@@ -1,37 +1,54 @@
-def process_text(raw_text):
-    """
-    Given raw text (e.g. from a file)
-    split on newlines so we have an array
-    of lines of text
-    """
-    return raw_text.strip().split('\n')
+import numpy
 
-def all_upper(text):
+class Layout(object):
     """
-    Make every line of text in an array uppercase
+    The Layout class handles arranging letters
+    in world space given a font and a set of desired
+    text.
     """
-    return [line.upper() for line in text]
+    # The font was based on cells of this size
+    CHAR_WIDTH = 3.0
+    CHAR_HEIGHT = 5.0
+    
+    # Space between rows
+    X_SPACE = 1.0
+    Y_SPACE = 2.0
 
-def pad_input(text):
-    """
-    Pad all strings in an array so they are the same length,
-    justified left.
-    """
-    width = max(len(line) for line in text)
-    return [line.ljust(width) for line in text]
+    def __init__(self, font, text):
+        self.font = font
+        self.text = text
 
-def display_text(text):
-    """
-    Display each line of text with vertical bars around it
-    to denote the size of the text
-    """
-    for line in text:
-        print '|{}|'.format(line)
+    @property
+    def width(self):
+        """Compute the width of the entire layout"""
+        N = self.text.cols
+        return N * self.CHAR_WIDTH + (N - 1) * self.X_SPACE
 
-raw = """
-Hello World
-My Name is Peter
-This is a fractal
-"""
+    @property
+    def height(self):
+        """Compute the height of the entire layout"""
+        M = self.text.rows
+        return M * self.CHAR_HEIGHT + (M - 1) * self.Y_SPACE
 
-display_text(pad_input(all_upper(process_text(raw))))
+    @property
+    def bounding_box(self):
+        """
+        Compute three points of the bounding box
+        as a numpy array in the form.
+
+        The bounding box is centered around the origin. Y is upwards
+
+        Y-------+
+        |       |
+        O-------X
+
+        [O_x, X_x, Y_x]
+        [O_y, X_y, Y_y]
+        """
+        half_w = self.width / 2.0
+        half_h = self.height / 2.0
+
+        return numpy.array([
+            [-half_w, half_w, -half_w],
+            [-half_h, -half_h, half_h]
+        ])
--- prototype.py
+++ prototype.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from text import Text
 from font import Font
+from layout import Layout
 
 txt = Text('Hello Cruel\n World!')
 print(txt)
 fnt = Font()
 txt.sanitize(fnt.alphabet)
 print(txt)
+
+lyt = Layout(fnt, txt)
+print(lyt.bounding_box)
