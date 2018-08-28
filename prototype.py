2487f95ac9deb2d2391c4785c53514031e7ae06d
Peter Gagliardi <ptrgags@gmail.com>
2018-08-28 11:58:29 +0000

    Add argparse arguments

--- prototype.py
+++ prototype.py
@@ -1,10 +1,57 @@
 #!/usr/bin/env python
+from __future__ import print_function
+import argparse
+
 from text import Text
 from font import Font
 from layout import Layout
 from affine_finder import AffineFinder
 from flame_writer import FlameWriter
 
+def make_flame(args):
+    """
+    Use the arguments to make a flame file.
+    """
+    # Select the padding style
+    pad_char = '█' if args.block_padding else ' '
+    text = Text(args.text, pad_char)
+
+    # Read in the font and sanitize the text
+    font = Font()
+    text.sanitize(font.alphabet)
+
+    print("Making fractal for the following text:")
+    print(text)
+
+
+    layout = Layout(font, text)
+    finder = AffineFinder(layout.bounding_box)
+    xforms = finder.find_xforms(layout.geometry)
+    writer = FlameWriter()
+    writer.write(args.fname, args.flame_name, xforms)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'fname',
+        help='Enter a path to the output .flame file')
+    parser.add_argument(
+        'flame_name',
+        help='Enter a name for the flame inside the pack')
+    parser.add_argument(
+        'text',
+        help='Text of the fractal. Use $\'these\\nstrings\' for multiline')
+    parser.add_argument(
+        '-b', 
+        '--block-padding', 
+        action='store_true', 
+        help='Use █ to fill out the empty spaces in the text')
+    return parser.parse_args()
+
+
+
+
+'''
 txt = Text('abcdefg\nhijklmn\nopqrst\nuvwxyz','█')
 print(txt)
 fnt = Font()
@@ -19,3 +66,9 @@ xforms = finder.find_xforms(lyt.geometry)
 
 writer = FlameWriter()
 writer.write('output/test.flame', 'Peter', xforms)
+'''
+
+if __name__ == '__main__':
+    args = get_args()
+    print(args.text)
+    make_flame(args)
